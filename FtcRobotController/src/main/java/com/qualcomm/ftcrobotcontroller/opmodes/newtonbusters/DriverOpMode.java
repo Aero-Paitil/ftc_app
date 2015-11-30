47309d36eb2ee1202ea066ac7c6bf9f5b44b9062
Sangmin Lee <bellybrown2001@gmail.com>
2015-11-29 18:11:05 -0600

    Added Rear Wheel Deployment and unDeployment

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/newtonbusters/DriverOpMode.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/newtonbusters/DriverOpMode.java
@@ -1,24 +1,38 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.newtonbusters;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 
 /**
  * Created by Aryoman on 11/24/2015.
  */
 public class DriverOpMode extends OpMode{
+    final static private int REAR_WHEELS_COUNTS = 1000;
+
     MecanumWheels mecanumWheels;
     Brushes brushes;
+    DcMotor rearWheels;
 
     @Override
     public void init() {
         mecanumWheels = new MecanumWheels(hardwareMap, telemetry, true); //We are using the Gyro.
         brushes = new Brushes (hardwareMap ,telemetry);
+        rearWheels = hardwareMap.dcMotor.get("RearWheels");
+        rearWheels.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    }
+
+    @Override
+    public void start(){
+        rearWheels.setTargetPosition(-REAR_WHEELS_COUNTS);
+        rearWheels.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        rearWheels.setPower(0.3);
     }
 
     @Override
     public void loop() {
 
-        while(gamepad1.a && gamepad2.b){
+        while(gamepad1.a && gamepad1.b){
             mecanumWheels.resetGyroHeading();
         }
 
@@ -29,8 +43,10 @@ public class DriverOpMode extends OpMode{
         double fieldForward = -gamepad1.left_stick_y; //field coordinates
         double fieldRight = gamepad1.left_stick_x; //field coordinates
         double clockwise = gamepad1.right_stick_x; //doesn't matter field or robot
-        mecanumWheels.powerMotors(fieldForward,fieldRight,clockwise,true);
+        mecanumWheels.powerMotors(fieldForward, fieldRight, clockwise, true);
 
+
+        //this code controls the brushes
         brushes.printTelemetry();
 
         if (gamepad2.dpad_up) {
@@ -48,5 +64,13 @@ public class DriverOpMode extends OpMode{
             brushes.setRotation(Brushes.STOP_ROTATION);
         }
 
+        //this code controls the rear wheels
+        telemetry.addData("rear wheel position", rearWheels.getCurrentPosition());
+        if (gamepad1.a){
+            rearWheels.setTargetPosition(rearWheels.getCurrentPosition() - 1000);
+        }
+        if (gamepad1.b){
+            rearWheels.setTargetPosition(rearWheels.getCurrentPosition() + 1000);
+        }
     }
 }
