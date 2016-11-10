952ed1fb8490204f47c1e0afe1dfbb55d35e3f13
jasmine2000 <jasmine2000@gmail.com>
2016-11-10 13:26:55 -0600

    Gavin: Added the sweeper intake and sweeper out. Intake : X, Sweeper Out : B

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverMode.java
@@ -49,6 +49,10 @@ public class DriverMode extends OpMode {
     private DcMotor motorRight1;
     private DcMotor motorLeft2;
     private DcMotor motorRight2;
+    private DcMotor motorBrush;
+    private boolean inBrushBtnPressed = false;
+    private boolean outBrushBtnPressed = false;
+    private int brushState = 0; //-1:Intake, 1:Sweep out, 0:Stopped
 
     private boolean forward;
     private boolean backButtonPressed = false;
@@ -66,6 +70,7 @@ public class DriverMode extends OpMode {
         motorRight1 = hardwareMap.dcMotor.get("D1right");
         motorLeft2 = hardwareMap.dcMotor.get("D2left");
         motorRight2 = hardwareMap.dcMotor.get("D2right");
+        motorBrush = hardwareMap.dcMotor.get("Brush");
 
         //setting the motors on the right side in reverse so both wheels spin the same way.
         motorLeft1.setDirection(DcMotor.Direction.REVERSE);
@@ -85,25 +90,52 @@ public class DriverMode extends OpMode {
     @Override
     public void loop() {
 
+        //Changing Brush State
+        if (gamepad1.x){
+            inBrushBtnPressed = true;
+        } else if (gamepad1.b){
+            outBrushBtnPressed = true;
+        }else{
+            if (inBrushBtnPressed){
+                inBrushBtnPressed = false;
+                brushState = (brushState==0) ? -1 : 0;
+            }
+            else if (outBrushBtnPressed) {
+                outBrushBtnPressed = false;
+                brushState = (brushState == 0) ? 1 : 0;
+            }
+        }
+        if (brushState == -1){
+            motorBrush.setPower(1);
+            telemetry.addData("Brush", "Intake");
+        }
+        else if (brushState == 1){
+            motorBrush.setPower(-1);
+            telemetry.addData("Brush", "Sweep Out");
+        } else{
+            motorBrush.setPower(0);
+            telemetry.addData("Brush", "Stopped");
+        }
 
         double leftForward;
         double rightForward;
-        int sign = forward ? 1 : -1;
 
+        int sign = forward ? 1 : -1;
         if (!gamepad1.left_bumper && !gamepad1.right_bumper){
+            //Arcade Drive
             rightForward = -(scaled(gamepad1.left_stick_y) + sign*scaled(gamepad1.left_stick_x));
             leftForward = -(scaled(gamepad1.left_stick_y) - sign*scaled(gamepad1.left_stick_x));
         }
         else {
+            //Tank Drive
             leftForward = -scaled(gamepad1.left_stick_y);
             rightForward = -scaled(gamepad1.right_stick_y);
         }
 
         if ((gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right)) {
+            // dPadDrive: Forward, Backwards, Rotate in place CClockwise and Clockwise
             //We are using robot coordinates
-
             double dpadSpeed = 0.2;
-
             if (gamepad1.dpad_up) {
                 rightForward = dpadSpeed;
                 leftForward = dpadSpeed;
@@ -140,24 +172,22 @@ public class DriverMode extends OpMode {
         rightForward = Range.clip(rightForward, -1, 1);
         leftForward = Range.clip(leftForward, -1, 1);
 
+        //Changing direction from forward to backward and backward to forward
+        //Gamepad back button does not work with motorola 3G
         if (gamepad1.y) {
             backButtonPressed = true;
         } else if (backButtonPressed) {
             backButtonPressed = false;
-
-            if (!forward){
-                forward = true;
-            } else {
-                forward = false;
-            }
+            forward = !forward;
         }
 
 
         telemetry.addData("left",leftForward);
         telemetry.addData("right",rightForward);
 
-        telemetry.addData("back button pressed", backButtonPressed);
+        telemetry.addData("back(Y) button pressed", backButtonPressed);
         telemetry.addData("forward", forward);
+
 
         /* assigning the motors the scaled powers that we just calculated in the step above. */
         powerMotors(rightForward, leftForward);
