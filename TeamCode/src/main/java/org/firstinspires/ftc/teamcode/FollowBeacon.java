c3000f07ea1e1e01eeadbd3ac24e241ee333ed57
purplecheese <jasmine2000@gmail.com>
2017-03-29 12:11:40 -0500

    Athena -- fixed the logic so program works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FollowBeacon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FollowBeacon.java
@@ -1,4 +1,7 @@
 package org.firstinspires.ftc.teamcode;
+
+import android.os.Environment;
+
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,27 +9,41 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 import java.io.IOException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+
 /**
  * Created by Athena on 3/25/2017.
  * will be following blue color, not red color
  */
+
 @TeleOp(name="FollowBeacon", group="nb")
+
 public class FollowBeacon extends OpMode {
+
     double DRIVE_POWER = 0.225;
+
     DcMotor motorLeft, motorRight;
+
     ColorSensor color3a, color3c;
     ModernRoboticsI2cRangeSensor range; //range 1 - 255 cm
-    BufferedWriter textFile;
-    StringBuffer line = new StringBuffer();
+
+    BufferedWriter bufferedWriter;
+    StringBuffer line = new StringBuffer("3a Red // 3a Blue // 3c Red // 3c Blue\n");
+
+    ElapsedTime time = new ElapsedTime();
+
     @Override
     public void init() {
         motorLeft = hardwareMap.dcMotor.get("D1left");
         motorRight = hardwareMap.dcMotor.get("D1right");
         motorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
         color3a = hardwareMap.colorSensor.get("Color Sensor 3a");
         color3a.setI2cAddress(I2cAddr.create8bit(0x3a));
         color3a.enableLed(false);
@@ -34,58 +51,57 @@ public class FollowBeacon extends OpMode {
         color3c.setI2cAddress(I2cAddr.create8bit(0x3c));
         color3c.enableLed(false);
         range = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "Range Sensor");
+
         try {
-            textFile = new BufferedWriter(new FileWriter("follow_beacon")); //DATE TIME
+            bufferedWriter = new BufferedWriter(new FileWriter(Environment.getExternalStorageDirectory().getPath() + "/FIRST/followBeacon.txt")); //DATE TIME
         } catch (IOException e) {
             e.printStackTrace();
         }
-        line.append("3a Red // 3a Blue // 3c Red // 3c Blue\\n");
     }
+
     @Override
     public void loop() {
-        int rangeFromBeacon = (int) range.getDistance(DistanceUnit.CM);
-        if (rangeFromBeacon < 10) {
+       long time = System.currentTimeMillis();
+        int minRange = (int) range.getDistance(DistanceUnit.CM);
+        if (minRange < 20 || color3a.blue() < 1 && color3c.blue() <1) {
             drive(0, 0);
-        } else if (color3a.blue() > color3a.red() && color3c.blue() > color3c.red()) {
+        } else if (color3a.blue() > 0 && color3c.blue() > 0) { //(color3a.blue() > color3a.red() && color3c.blue() > color3c.red()) {
             drive(DRIVE_POWER, DRIVE_POWER);
-            writeToLine();
-        } else if (color3a.blue() > color3a.red() && color3c.blue() < color3c.red()) {
+            //while (time < 1000) {
+                ;
+            //}
+           writeToLine();
+        } else if (color3a.blue() > color3c.blue() || color3a.blue() > 0 && color3c.blue() == 0) {//(color3a.blue() > color3a.red() && color3c.blue() < color3c.red()) {
             drive(DRIVE_POWER, 0);
+            //while (time < 1000) {
+                ;
+            //}
             writeToLine();
-        } else if (color3a.blue() < color3a.red() && color3c.blue() > color3c.red()) {
+        } else if (color3a.blue() < color3c.blue() || color3a.blue() == 0 && color3c.blue() > 0) { //(color3a.blue() < color3a.red() && color3c.blue() > color3c.red()) {
             drive(0, DRIVE_POWER);
+            //while (time < 1000) {
+                ;
+            //}
             writeToLine();
         }
     }
+
     @Override
     public void stop() { //write to file
-        drive(0, 0);
         try {
-            textFile.write(line.toString());
+            bufferedWriter.write(line.toString());
+            bufferedWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     void drive(double powerLeft, double powerRight) {
         motorLeft.setPower(-powerLeft);
         motorRight.setPower(-powerRight);
     }
+
     void writeToLine() {
-        double red3a = color3a.red();
-        double blue3a = color3a.blue();
-        double red3c = color3c.red();
-        double blue3c = color3c.blue();
-        try {
-            line.append(red3a + " // " + blue3a + " // " + red3c + " // " + blue3c + "\n");
-        } finally {
-            if (textFile != null) {
-                try {
-                    textFile.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        line.append(color3a.red() + " // " + color3a.blue() + " // " + color3c.red() + " // " + color3c.blue() + "\n");
     }
 }
-
